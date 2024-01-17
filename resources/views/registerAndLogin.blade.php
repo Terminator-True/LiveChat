ed0b1e2167b1e298737335d18586cb4eb39eb1e5
Terminator-True <jf28renault@gmail.com>
2024-01-17 13:02:12 +0100

    FIX: Register view FIX

--- resources/views/registerAndLogin.blade.php
+++ resources/views/registerAndLogin.blade.php
@@ -4,50 +4,106 @@
 @section('content')
 <div style="height: 100%" class="container d-flex align-items-center justify-content-center">
     <div id="register" class="container" style="margin: auto">
+
+        @if($errors->any())
+            <div class="register-page">
+                <div class="form">
+                        <form method="POST"  action="{{ route('user.register') }}" class="" id='register-form'>
+                            @csrf
+                            <input class="@error('name') is-invalid @enderror" type="text" placeholder="name" name='name'/>
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            <input class="@error('nick') is-invalid @enderror" type="text" placeholder="nick" name='nick'/>
+
+                            @error('nick')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+
+                            <input class="@error('password') is-invalid @enderror" type="password" placeholder="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required name='password'/>
+
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+
+                            <input class="@error('email') is-invalid @enderror" type="text" placeholder="email address"name='email'/>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+
+                            <button class="button_piu">create</button>
+                            <p class="message">Already registered? <a href="#">Sign In</a></p>
+                        </form>
+
+                        <form method="POST" action="{{ route('user.login') }}" class="deshabilited-form" id="login-form">
+                            @csrf
+                            <input type="text" placeholder="email" name='email'/>
+                            <input type="password" placeholder="password" name='password'/>
+                            <button class="button_piu">login</button>
+                            <p class="message">Not registered? <a href="#">Create an account</a></p>
+                        </form>
+                </div>
+            </div>
+            @isset($error)
+                <div class="alert">
+                    <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                    <strong>Error!</strong> {{ $error }}
+                </div>
+            @endisset
+
+        @else
         <div class="login-page">
             <div class="form">
-            <form method="POST"  action="{{ route('user.register') }}" class="register-form">
-                @csrf
-                <input class="@error('name') is-invalid @enderror" type="text" placeholder="name" name='name'/>
-                    @error('name')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                <input class="@error('nick') is-invalid @enderror" type="text" placeholder="nick" name='nick'/>
+                    <form method="POST"  action="{{ route('user.register') }}" class="deshabilited-form" id='register-form'>
+                        @csrf
+                        <input class="@error('name') is-invalid @enderror" type="text" placeholder="name" name='name'/>
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        <input class="@error('nick') is-invalid @enderror" type="text" placeholder="nick" name='nick'/>
 
-                @error('nick')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
+                        @error('nick')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
 
-                <input class="@error('password') is-invalid @enderror" type="password" placeholder="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required name='password'/>
+                        <input class="@error('password') is-invalid @enderror" type="password" placeholder="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required name='password'/>
 
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
 
-                <input class="@error('email') is-invalid @enderror" type="text" placeholder="email address"name='email'/>
-                @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
+                        <input class="@error('email') is-invalid @enderror" type="text" placeholder="email address"name='email'/>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
 
-                <button class="button_piu">create</button>
-                <p class="message">Already registered? <a href="#">Sign In</a></p>
-            </form>
+                        <button class="button_piu">create</button>
+                        <p class="message">Already registered? <a href="#">Sign In</a></p>
+                    </form>
 
-            <form method="POST" action="{{ route('user.login') }}" class="login-form">
-                @csrf
-                <input type="text" placeholder="email" name='email'/>
-                <input type="password" placeholder="password" name='password'/>
-                <button class="button_piu">login</button>
-                <p class="message">Not registered? <a href="#">Create an account</a></p>
-            </form>
+                    <form method="POST" action="{{ route('user.login') }}" class="" id="login-form">
+                        @csrf
+                        <input type="text" placeholder="email" name='email'/>
+                        <input type="password" placeholder="password" name='password'/>
+                        <button class="button_piu">login</button>
+                        <p class="message">Not registered? <a href="#">Create an account</a></p>
+                    </form>
             </div>
         </div>
         @isset($error)
@@ -56,6 +112,10 @@
                 <strong>Error!</strong> {{ $error }}
             </div>
         @endisset
+
+    @endif
+
+
     </div>
 </div>
 
@@ -63,24 +123,9 @@
 <script>
 
 $('.message a').click(function(){
+
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 
-   if (localStorage.getItem('form') == undefined || localStorage.getItem('form') == 'login') {
-        localStorage.setItem('form','register')
-   } else {
-       localStorage.setItem('form','login')
-   }
-});
-
-window.addEventListener("load", (event) => {
-    setTimeout(() => {
-        let form = localStorage.getItem('form');
-        if(form == 'register')
-        {
-            $('form').animate({height: "toggle", opacity: "toggle"});
-
-        }
-    }, 200);
 });
 
 </script>
